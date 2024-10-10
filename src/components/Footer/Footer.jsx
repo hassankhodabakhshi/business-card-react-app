66ddf455a2a74151d5521a911f3e438461e8e5a7
hassan khodabakhshi <80225351+hassankhodabakhshi@users.noreply.github.com>
2024-10-10 18:13:20 +0330

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,19 +7,19 @@ export default function Footer(){
     return(
         <footer class="footer">
             <div class="footer-item">
-                <a href="https://twitter.com/pritisolanki" target="_blank"><FontAwesomeIcon icon={brands('twitter')} /></a>
+                <a href="https://twitter.com/h_khodabakhshi" target="_blank"><FontAwesomeIcon icon={brands('twitter')} /></a>
             </div>
             <div>
-                <FontAwesomeIcon icon={brands('instagram')} />
+                <a href="https://www.facebook.com/hassankhodabakhshi" target="_blank"><FontAwesomeIcon icon={brands('instagram')} />
             </div>
             <div>
-                <FontAwesomeIcon icon={brands('facebook')} />
+                <a href="https://www.instagram.com/hassankhodabakhshi" target="_blank"><FontAwesomeIcon icon={brands('facebook')} />
             </div>
             <div>
-                <a href="https://www.linkedin.com/in/priti-s-43a2b8239/" target="_blank"><FontAwesomeIcon icon={brands('linkedin')} /></a>
+                <a href="https://www.linkedin.com/in/hassan-khodabakhshi-495743a0" target="_blank"><FontAwesomeIcon icon={brands('linkedin')} /></a>
             </div>
             <div>
-                <a href="https://github.com/pritisolanki" target="_blank"><FontAwesomeIcon icon={brands('github')} /></a>
+                <a href="https://github.com/hassankhodabakhshi" target="_blank"><FontAwesomeIcon icon={brands('github')} /></a>
             </div>
         </footer>
     )
