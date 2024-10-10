26bf592ca53f4578d2042b70b56b3b0bb2de5dc2
hassan khodabakhshi <80225351+hassankhodabakhshi@users.noreply.github.com>
2024-10-10 18:08:05 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,14 @@ function App() {
               <img src={userAvatar} className="avatar" alt="React logo" />
             </div>
             <div>
-              <h2 className="name">Priti Solanki</h2>
-              <h3 className="designation">Solution Architect</h3>
-              <h4 className="website"><a href="https://pritisolanki.com/" target="_blank">pritisolanki.com</a></h4>
-              <button><a href="mailto:norpely@donotspam.com"><img src={mail} className="emailIcon" /><span class="btntext">Email</span></a></button>
+              <h2 className="name">hassan khodabakhshi</h2>
+              <h3 className="designation">Site designer</h3>
+              <h4 className="website"><a href="https://pritisolanki.com/" target="_blank">isonetwork.ir</a></h4>
+              <button><a href="mailto:hasankhodabakhshi@gmail.com"><img src={mail} className="emailIcon" /><span class="btntext">Email</span></a></button>
               <h2 class="remove-space">About</h2>
-              <p>I am a solution architect with a particular interest in making things simple. I try to keep up with frontend technologies and best practices, and am always looking for new things to learn.</p>
+              <p>I am a web designer and a computer technician, and I am interested in developing websites.</p>
               <h2 class="remove-space">Interests</h2>
-              <p class="remove-space">Food expert. Music scholar. Reader. Blogger. [Copied]</p>
+              <p class="remove-space">Food expert. Music scholar. Reader. Programming. [Copied]</p>
             </div>
             <Footer />
           </div>
